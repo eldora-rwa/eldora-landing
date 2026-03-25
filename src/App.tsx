95602b462b08803ea766790bf869154c9025b5e7
minhkhoi8888 <hoangminhkhoi245@gmail.com>
2026-03-25 11:53:47 +0700

    init new landingpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
-import LandingPage from "./pages/LandingPage";
+// import LandingPage from "./pages/LandingPage";
 import EldoraPage from "./pages/EldoraPage";
+import LandingPageNew from "./pages/LandingPageNew";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<LandingPageNew />} />
         <Route path="/eldora" element={<EldoraPage />} />
       </Routes>
     </BrowserRouter>
